a146d6970505d9ecac42777452f7851b18a162cf
koredeycode <koredey4u@gmail.com>
2023-05-17 22:00:17 +0000

    four

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Module documentation
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    inherits from BaseCaching. A caching system
+    """
+    def __init__(self):
+        """
+        initialization
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                k, v = self.cache_data.popitem()
+                print("DISCARD: {}".format(k))
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in the cache data linked to key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=True)
+        return self.cache_data.get(key, None)
