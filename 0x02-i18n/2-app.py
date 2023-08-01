9ad5218e3dc1a13f91325f348e9aebc6afb3bc16
koredeycode <koredey4u@gmail.com>
2023-08-01 19:30:06 +0000

    this

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+The main flask application file
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Represent a Flask babel configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for the user's preferred language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index() -> str:
+    """
+    the index page
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
