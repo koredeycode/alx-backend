e2982b3af0f9673d4c3893825eeded5857ec88f2
koredeycode <koredey4u@gmail.com>
2023-08-01 19:24:32 +0000

    pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,7 +5,6 @@ The main flask application file
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
 
 class Config:
     """
@@ -15,8 +14,11 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route("/")
 def index() -> str:
